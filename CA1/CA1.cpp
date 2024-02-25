5a45dc8bdff0a52b5dea4b7532a094ec87781ca4
Amir-rfz <amirrfz2003@gmail.com>
2024-02-25 16:05:08 +0330

    adding some comment to CA1

--- CA1/CA1.cpp
+++ CA1/CA1.cpp
@@ -2,11 +2,15 @@
 #include<string>
 #include<vector>
 using namespace std;
+
+//making a structur for each lesson we want to get this term
 struct lessons{
     string course_name;
     int units;
     string prerequisites;
 };
+
+//this function gets the lesson that Offered in this term from input
 vector<lessons> get_this_term_lesson(int num){
     vector<lessons> result;
     for(int i=0;i<num;i++){
@@ -16,11 +20,15 @@ vector<lessons> get_this_term_lesson(int num){
     }
     return result;
 }
+
+//making a structur for each lesson we had in past
 struct past_lessons{
     string course_name;
     int units;
     double grade;
 };
+
+//this function gets the lessons that we had in past from input
 vector<past_lessons> get_past_lesson(int num){
     vector<past_lessons> result;
     for(int i=0;i<num;i++){
@@ -30,6 +38,8 @@ vector<past_lessons> get_past_lesson(int num){
     }
     return result;
 }
+
+//this function gets the lesson that we want to get in upcoming term
 vector<string>get_wanted_lessons(int num){
     vector<string> result;
     for (int i=0;i<num;i++){
@@ -39,6 +49,8 @@ vector<string>get_wanted_lessons(int num){
     }
     return result;
 }
+
+//this function check that the course that we want is offered this term or not
 bool check_ofered(vector<string> wanted_lessons,vector<lessons> this_term_lesson){
     for(int i=0;i<wanted_lessons.size();i++){
         bool ofered=false;
@@ -51,6 +63,8 @@ bool check_ofered(vector<string> wanted_lessons,vector<lessons> this_term_lesson
     }
     return true;
 }
+
+//this function count our units that we want to get this term
 int number_of_unit(vector<string> wanted_lessons,vector<lessons> this_term_lesson){
     int units=0;
     for(int i=0;i<wanted_lessons.size();i++){
@@ -61,6 +75,8 @@ int number_of_unit(vector<string> wanted_lessons,vector<lessons> this_term_lesso
     }
     return units;
 }
+
+//this function check that is our units is more than 12 or not
 bool check_min_units(vector<string> wanted_lessons,vector<lessons> this_term_lesson){
     int units=number_of_unit(wanted_lessons,this_term_lesson);
     if(units>=12)
@@ -68,6 +84,8 @@ bool check_min_units(vector<string> wanted_lessons,vector<lessons> this_term_les
     else
         return false;
 }
+
+//this function calculate our GPA 
 double GPA(vector<past_lessons> past_lesson){
     double sum=0;
     for(int i=0;i<past_lesson.size();i++){
@@ -76,6 +94,8 @@ double GPA(vector<past_lessons> past_lesson){
     double GPA=sum/past_lesson.size();
     return GPA;
 }
+
+//this function check that we dont get more than maximum utit we can get with our GPA
 bool check_max_units(vector<lessons> this_term_lesson,vector<past_lessons> past_lesson,vector<string> wanted_lessons){
     double gpa=GPA(past_lesson);
     int unit_number=number_of_unit(wanted_lessons,this_term_lesson);
@@ -88,6 +108,8 @@ bool check_max_units(vector<lessons> this_term_lesson,vector<past_lessons> past_
     else    
         return false;
 }
+
+//this function check that is a course taken before or not
 bool is_taken_course(vector<string> wanted_lessons,vector<past_lessons> past_lesson){
     bool taken;
     for(int i=0;i<wanted_lessons.size();i++){
@@ -103,6 +125,8 @@ bool is_taken_course(vector<string> wanted_lessons,vector<past_lessons> past_les
     }
     return false;
 }
+
+//this function splite the Prerequisites from ',' to a vector of strings
 vector<string> splite_Prerequisites(string Prerequisites){
     vector<string> result;
     string Prerequisite="";
@@ -118,6 +142,8 @@ vector<string> splite_Prerequisites(string Prerequisites){
     result.push_back(Prerequisite);
     return result;
 }
+
+//this function return a vector of all course we passed 
 vector<string>get_pass_lesssons(vector<past_lessons> past_lessons){
     vector<string>pass_lessons;
     for(auto past_lesson:past_lessons){
@@ -126,6 +152,8 @@ vector<string>get_pass_lesssons(vector<past_lessons> past_lessons){
     }
     return pass_lessons;
 }
+
+//this function return all Prerequisites that a course had in a string in form: <name1,name2,...>
 string get_Prerequisites(string lesson_name,vector<lessons> this_term_lesson){
     for(auto lesson : this_term_lesson){
         if(lesson_name==lesson.course_name)
@@ -133,6 +161,8 @@ string get_Prerequisites(string lesson_name,vector<lessons> this_term_lesson){
     }
     return " ";
 }
+
+//this function check that did we pass the Prerequisite or not
 bool Prerequisites_isnt_Met(string Prerequisite ,vector<string>pass_lessons){
     for(auto pass_lesson : pass_lessons){
         if(pass_lesson==Prerequisite)
@@ -140,6 +170,8 @@ bool Prerequisites_isnt_Met(string Prerequisite ,vector<string>pass_lessons){
     }
     return true;
 }
+
+//this function check that did we pass all Prerequisites of lessons we want to get this term
 bool check_Prerequisites(vector<string> wanted_lessons,vector<past_lessons> past_lesson,vector<lessons> this_term_lesson){
     vector<string>pass_lessons=get_pass_lesssons(past_lesson);
     for(int i=0;i<wanted_lessons.size();i++){
@@ -152,22 +184,27 @@ bool check_Prerequisites(vector<string> wanted_lessons,vector<past_lessons> past
     }
     return true;
 }
+
 int main(){
     vector<lessons> this_term_lesson;
     vector<past_lessons> past_lesson;
     vector<string> wanted_lessons;
+
     //getting the lessons for upcoming term
     int num_of_this_term_lesson;
     cin>>num_of_this_term_lesson;
     this_term_lesson=get_this_term_lesson(num_of_this_term_lesson);
+
     //getting the lessons that we had 
     int num_of_past_lesson;
     cin>>num_of_past_lesson;
     past_lesson= get_past_lesson(num_of_past_lesson);
+
     //get the lessons we want to get this term
     int num_of_wanted_lesson;
     cin>>num_of_wanted_lesson;
     wanted_lessons= get_wanted_lessons(num_of_wanted_lesson);
+
     //check if we can get this lessons or not
     if(!check_ofered(wanted_lessons,this_term_lesson))
         cout<<"Course Not Offered!";
@@ -181,5 +218,6 @@ int main(){
         cout<<"Prerequisites Not Met!";
     else
         cout<<"OK!";
+
     return 0;
 }
